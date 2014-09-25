7fa8d1946ff2cdc0a9a3f41d75ffd9cbbf12745b
Sergii Mertsalov <student@epicodus.com>
2014-09-25 11:20:25 -0700

    build an api for show and index

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,19 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @movies }
+    end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @movie }
+    end
+  end
+end
